e721aab9f8cec1571144e89f97739960175c9b8a
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-04-16 23:55:46 +0530

    Add Message Entity

---
+++ backend/src/main/java/com/shayarify/backend/model/Message.java
@@ -0,0 +1,27 @@
+package com.shayarify.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor//default cnstr
+@AllArgsConstructor//depd Inj
+@Builder//lombok
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long senderId;
+    private Long receiverId;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime timestamp;
+}
