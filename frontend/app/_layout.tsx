305a648c3caebcc7b2b50c3170d60ef4fddce0e2
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-04-20 23:50:15 +0530

    Add root layout

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,7 +1,25 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "./src/context/AuthContext";
+import {loadFonts} from "@/app/src/hooks/useFonts";
+import {useEffect, useState} from "react";
 
 export default function RootLayout() {
+    const [ready, setReady] = useState(false);
+
+    useEffect(() => {
+        const init = async () => {
+            await loadFonts();
+            setReady(true);
+            console.log("Ready!");
+        };
+
+        init();
+    }, []);
+
+    if (!ready) return null; // or loader
+
+    // return <Slot />;
+
   return (
       <AuthProvider>
         <Stack screenOptions={{ headerShown: false }} />
