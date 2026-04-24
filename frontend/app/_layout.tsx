5f070ca25c7bee19d4251adb970321594c11366d
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-04-24 23:55:38 +0530

    modify comments in root layout

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,14 +3,16 @@ import { AuthProvider } from "./src/context/AuthContext";
 import {loadFonts} from "@/app/src/hooks/useFonts";
 import {useEffect, useState} from "react";
 
+//Skeletal structure of the app
 export default function RootLayout() {
     const [ready, setReady] = useState(false);
 
+    //just loading fonts when app opens
     useEffect(() => {
         const init = async () => {
             await loadFonts();
             setReady(true);
-            console.log("Ready!");
+            console.log("READY TO GO! APP HAS BEEN STARTED....");
         };
 
         init();
@@ -18,6 +20,7 @@ export default function RootLayout() {
 
     if (!ready) return null; // or loader
 
+//Wrap with auth provider from auth context
   return (
       <AuthProvider>
         <Stack screenOptions={{ headerShown: false }} />
