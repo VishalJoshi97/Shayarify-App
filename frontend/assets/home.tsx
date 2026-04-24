6f5b93b455c3254755a6e1b6b3963ddc7409b37c
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-04-24 23:58:09 +0530

    move home screen to zextra

--- frontend/assets/home.tsx
+++ frontend/assets/home.tsx
@@ -1,13 +1,9 @@
 import { View, Text, Button } from "react-native";
-import { useAuth } from "../src/hooks/useAuth";
 
 export default function Home() {
-    const { logout } = useAuth();
-
     return (
         <View>
             <Text>Home Screen</Text>
-            <Button title="Logout" onPress={logout} />
         </View>
     );
 }
