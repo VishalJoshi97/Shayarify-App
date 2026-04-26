e8815453e3a6c7d0e596f95d4ce9f6f53c5b211c
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-04-26 23:59:38 +0530

    create profile.tsx

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,17 +1,77 @@
-import { View, Text } from "react-native";
+import {Text, View, StyleSheet, TouchableOpacity} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import { useEffect, useState } from "react";
-import { getProfile } from "../src/api/profileApi";
+import { useAuth } from "@/app/src/hooks/useAuth";
+import { getProfile, updateProfile } from "../src/api/profileApi";
+import {FontAwesome, Ionicons} from '@expo/vector-icons';
+
+import ProfileHeader from "../../app/components/ProfileHeader";
+import BioSection from "../../app/components/BioSection";
+import QuoteRotator from "../../app/components/ QuoteRotator";
+import MusicPlayer from "../../app/components/MusicPlayer";
 
 export default function Profile() {
-    const [data, setData] = useState("");
+    const { user ,logout} = useAuth();
+
+    const [profile, setProfile] = useState({
+        bio: "",
+        imageUrl: "",
+    });
 
     useEffect(() => {
-        getProfile().then((res) => setData(res.data));
+        fetchProfile();
     }, []);
 
+    const fetchProfile = async () => {
+        const res = await getProfile();
+        setProfile(res.data);
+    };
+
+    const saveProfile = async (updated: any) => {
+        await updateProfile(updated);
+        setProfile(updated);
+    };
+
     return (
-        <View>
-            <Text>{data}</Text>
-        </View>
+        <LinearGradient
+            colors={["#0a0a0a", "#121212", "#000"]}
+            style={styles.container}
+        >
+             <ProfileHeader
+                username={user?.username || "User"}
+                profile={profile}
+                saveProfile={saveProfile}
+            />
+
+            <BioSection
+                profile={profile}
+                saveProfile={saveProfile}
+            />
+
+            <QuoteRotator />
+            <MusicPlayer />
+            <TouchableOpacity
+                onPress={logout}
+                style={{
+                    marginTop: 20,
+                    padding: 10,
+                    alignItems: "center"
+                }}
+            >
+                {/*<Text style={{ color: "red", fontWeight: "500" }}>*/}
+                {/*    Logout*/}
+                {/*</Text>*/}
+                <Ionicons name="log-out" size={24} color="blue" />
+
+            </TouchableOpacity>
+        </LinearGradient>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 70,
+        paddingHorizontal: 16,
+    },
+});
