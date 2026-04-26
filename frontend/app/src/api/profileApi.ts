51bc88fe1012973101b292c2c7d6b74fbe00844f
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-04-26 23:58:59 +0530

    create updateProfile and getProfileById APIs

--- frontend/app/src/api/profileApi.ts
+++ frontend/app/src/api/profileApi.ts
@@ -1,4 +1,24 @@
 import client from "./client";
+import {ProfileId, ProfileType} from "@/app/src/types/profile";
 
-export const getProfile = () => client.get("/profile/jwt");
-export const getOAuthProfile = () => client.get("/profile/oauth");
+export const createProfile = async (profileRequest:ProfileType) => {
+   const res=await client.post("/profile/create",profileRequest);
+   return res.data;
+}
+
+export const getProfile = async () => {
+    const res=await client.get("/profile/get");
+    return res;
+}
+
+export const updateProfile = async (update:ProfileType) => {
+   const res=await client.put("/profile/update",update);
+    console.log("UPDATED PROFILE DATA :",res.data)
+   return res.data;
+}
+
+export const getProfileById = async (id:number) => {
+    const res=await client.get(`/profile/get/${id}`);
+    console.log("SPECIFIC PROFILE : ",res.data)
+    return res.data;
+}
