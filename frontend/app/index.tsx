fbde27e5cba9e54bc6a246808980edb71a95d45c
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-04-30 23:56:52 +0530

    entry to app

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,26 +1,29 @@
 import { useEffect } from "react";
 import { useRouter } from "expo-router";
 import { View, Text, ActivityIndicator } from "react-native";
-import { useAuth } from "./src/hooks/useAuth";
-import {getToken} from "@/app/src/utils/storage";
+import { useAuth } from "../src/hooks/useAuth";
 
-export default function Index() {
+//Entry point of the app-aka splash screen
+//decides where to redirect based on-> jwt token info
+export default function Index(){
     const router = useRouter();
-    const { token, loading } = useAuth();
+    const { token, loading } = useAuth();//from AuthContext then useAuth updates states
 
     useEffect(() => {
-        if (!loading) {
-            if (token) {
-                router.replace("/(tabs)/profile");
+        if (!loading) {//loading should be false(from context)
+            if (token) {//now u have token
+                console.log("Already has token...")
+                router.replace("/(tabs)/feed")
             } else {
                 router.replace("/(auth)/login");
             }
         }
-        console.log("TOKEN: ",token);
-        console.log("LOADING: ",loading);
+        console.log("LOADING:  ",loading);
+        console.log("TOKEN:  ",token);
     }, [token, loading]);
 
 
+    //show the app logo for <= 2 sec
      return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
             <Text style={{ fontSize: 24 }}>Shayarify ✨</Text>
