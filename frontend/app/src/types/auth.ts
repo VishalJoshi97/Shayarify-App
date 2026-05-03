f350e24148fc3937e4683995087543335135c296
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-05-03 23:39:26 +0530

    create auth.ts

--- frontend/app/src/types/auth.ts
+++
@@ -1,9 +0,0 @@
-import {User} from "@/app/src/types/user";
-
-export type AuthContextType = {
-    token: string | null;
-    user: User | null;
-    loading: boolean;
-    login: (token: string, user: User) => Promise<void>;
-    logout: () => Promise<void>;
-};
