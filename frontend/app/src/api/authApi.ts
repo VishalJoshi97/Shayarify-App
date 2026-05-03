12fde48c59addd023289640ed975a0b4094fc477
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-05-03 23:41:40 +0530

    create authApi.ts

--- frontend/app/src/api/authApi.ts
+++
@@ -1,7 +0,0 @@
-import client from "./client";
-
-export const loginApi = (data: { email: string; password: string}) =>
-    client.post("/auth/login", data);
-
-export const registerApi = (data: any) =>
-    client.post("/auth/register", data);
