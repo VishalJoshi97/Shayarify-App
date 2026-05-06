7e61e22b593e7dfd6c1d8b4e23407c4e0b740dcf
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-05-06 19:33:58 +0530

    create user.ts

---
+++ frontend/src/types/user.ts
@@ -0,0 +1,10 @@
+// export type User = {
+//     id: number;
+//     email: string;
+//     name?: string;
+// };
+export type User = {
+    id: number;
+    username: string;
+    email: string;
+};
