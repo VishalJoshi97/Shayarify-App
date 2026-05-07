e000afb537d4b081a33f7c91f7c797a242a3d11d
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-05-07 14:00:56 +0530

    zextra: commit

---
+++ backend/src/main/java/com/shayarify/backend/zextra/ShayariService.java
@@ -0,0 +1,23 @@
+//package com.shayarify.backend.zextra;
+//
+//import com.shayarify.backend.model.Shayari;
+//import com.shayarify.backend.zextra.ShayariRepository;
+//
+//import java.util.List;
+//
+////@Service
+////@RequiredArgsConstructor
+//public class ShayariService {
+//
+//    private  final ShayariRepository shayariRepository;
+//
+//    //Entire shayari entity=>.save()
+//    public Shayari save(Shayari shayari){
+//        return shayariRepository.save(shayari);
+//    }
+//
+//    //getAll() => .findAll()
+//    public List<Shayari> getAll(){
+//        return shayariRepository.findAll();
+//    }
+//}
