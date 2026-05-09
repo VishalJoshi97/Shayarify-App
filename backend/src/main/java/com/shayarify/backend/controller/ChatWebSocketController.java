a4344f0019de999ad7d18ab26f490f705188d11d
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-05-09 16:41:55 +0530

    modify ChatWebSocketController for room chat feature

--- backend/src/main/java/com/shayarify/backend/controller/ChatWebSocketController.java
+++ backend/src/main/java/com/shayarify/backend/controller/ChatWebSocketController.java
@@ -1,10 +1,12 @@
 package com.shayarify.backend.controller;
 
+import com.shayarify.backend.dto.room.ChatMessage;
 import com.shayarify.backend.model.Message;
 import com.shayarify.backend.model.User;
 import com.shayarify.backend.repository.MessageRepository;
 import com.shayarify.backend.repository.UserRepository;
 import com.shayarify.backend.service.message.MessageService;
+import com.shayarify.backend.service.room.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -22,6 +24,8 @@ public class ChatWebSocketController {
 
      private final MessageRepository messageRepository;
 
+     private final RoomService roomService;
+
      private final UserRepository userRepository;
 
     @MessageMapping("/chat")
@@ -38,6 +42,27 @@ public class ChatWebSocketController {
 
         messagingTemplate.convertAndSend(
                 "/topic/messages/" + message.getSenderId(),
+                message
+        );
+    }
+
+    @MessageMapping("/chat.send")
+    public void sendMessage(
+            ChatMessage message,
+            Principal principal
+    ) {
+
+        User user = userRepository
+                .findByUsername(principal.getName())
+                .orElseThrow();
+
+        message.setSenderId(user.getId());
+        message.setSenderUsername(user.getUsername());
+
+        roomService.saveSocketMessage(message);
+
+        messagingTemplate.convertAndSend(
+                "/topic/rooms/" + message.getRoomCode(),
                 message
         );
     }
