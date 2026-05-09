4f0fd20479a32ea86a2f86b006584bb5a1a58c20
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-05-09 16:37:49 +0530

    create room model

---
+++ backend/src/main/java/com/shayarify/backend/model/Room.java
@@ -0,0 +1,52 @@
+package com.shayarify.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "rooms")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String roomName;
+
+    @Column(unique = true, nullable = false)
+    private String roomCode;
+
+    @Column(nullable = false)
+    private Long createdBy;
+
+    private LocalDateTime createdAt;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "room_members", joinColumns = @JoinColumn(name = "room_id"))
+    @Column(name = "user_id")
+    private Set<Long> members = new HashSet<>();
+
+    @PrePersist
+    public void prePersist() {
+
+        this.createdAt = LocalDateTime.now();
+
+        if (this.roomCode == null || this.roomCode.isEmpty()) {
+            this.roomCode = UUID.randomUUID().toString()
+                    .substring(0, 6)
+                    .toUpperCase();
+        }
+    }
+}
