1325fabeab46b8a68876faccd392c30588bf9424
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-05-09 16:40:42 +0530

    create dto JoinRoomRequest.java

---
+++ backend/src/main/java/com/shayarify/backend/dto/room/JoinRoomRequest.java
@@ -0,0 +1,11 @@
+package com.shayarify.backend.dto.room;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class JoinRoomRequest {
+    private String roomCode;
+}
