797d742ffb55e45cbcbd8c4e0693dbf8a422e51a
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-05-09 16:44:32 +0530

    set auth principal for room

--- backend/src/main/java/com/shayarify/backend/controller/RoomController.java
+++ backend/src/main/java/com/shayarify/backend/controller/RoomController.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//only authenticated users can participate in room activity
 @RestController
 @RequestMapping("/api/rooms")
 @RequiredArgsConstructor
