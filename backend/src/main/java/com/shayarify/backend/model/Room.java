9cf1b006c4ab8c37b5af172ad956a95788f06431
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-05-09 17:08:00 +0530

    add @Builder.Default

--- backend/src/main/java/com/shayarify/backend/model/Room.java
+++ backend/src/main/java/com/shayarify/backend/model/Room.java
@@ -33,8 +33,12 @@ public class Room {
 
     private LocalDateTime createdAt;
 
+    @Builder.Default
     @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "room_members", joinColumns = @JoinColumn(name = "room_id"))
+    @CollectionTable(
+            name = "room_members",
+            joinColumns = @JoinColumn(name = "room_id")
+    )
     @Column(name = "user_id")
     private Set<Long> members = new HashSet<>();
 
